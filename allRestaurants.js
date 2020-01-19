44ec19d55c0b043d42209cabe66c7f64a1323db0
Alex <nathaliealexandra@gmail.com>
2020-01-19 15:03:08 +0000

    the distance thing sorta works???

--- allRestaurants.js
+++ allRestaurants.js
@@ -11,12 +11,31 @@ var settings = {
     },
 };
 
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {//this gets the distance in crow flies
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1);
+    var a =
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+}
+
 function populateRestaurant() {
     // console.log("ran stuff");
         $.ajax(settings).done(function (response) {
         console.log(response);
         let branches = response;
-        let currentBranch, template, tbody, clone, tgt;
+        let currentBranch, template, tbody, clone, tgt, tdist, tride, lat, long, distanceFromUser, closestBranch, RiderNumber, queue;
+        let start = 1;
         for(let index = 0; index < branches.length; index++)
         {
             let list_entry = branches[index];
@@ -30,6 +49,23 @@ function populateRestaurant() {
 
                 // Instantiate the table with the existing HTML tbody
                 // and the row with the template
+
+                distanceFromUser = 40000; //order of length of equator: nothing can be as large as this distance
+                closestBranch = 0;
+                currentBranch = list_entry["restaurant_org"];
+
+                //console.log(Object.keys(list_entry["restaurant_branches"]).length);
+                for (let i = start; i < Object.keys(list_entry["restaurant_branches"]).length; i++){
+                    lat = parseFloat((list_entry["restaurant_branches"][i.toString()]["location"]["lat"]));
+                    long = parseFloat((list_entry["restaurant_branches"][i.toString()]["location"]["long"]));
+                    console.log(getDistanceFromLatLonInKm(lat,long,UserLat,UserLong));
+                    if (getDistanceFromLatLonInKm(lat,long,UserLat,UserLong) < distanceFromUser){
+                        distanceFromUser = getDistanceFromLatLonInKm(lat,long,UserLat,UserLong);
+                        closestBranch = i;
+                    }
+                }
+                start += Object.keys(list_entry["restaurant_branches"]).length + 1;
+                console.log(start);
                 template = document.querySelector('#restaurants');
 
                 // Clone the new row and insert it into the table
@@ -38,11 +74,11 @@ function populateRestaurant() {
                 //need to get the dropdown here
                 tbody = clone.querySelector("div.text-here");
                 tbody.textContent = currentBranch;
-                //tbody = clone.querySelector("div.dropdown-divider"); this line doesn't workkkkkkkkk
+                tdist = clone.querySelector("div.Distance-from-you");
+                tdist.textContent = distanceFromUser + "km away";
                 // console.log(clone);
 
                 tgt = document.querySelector("#contains-restaurants");
-
                 tgt.appendChild(clone);
             // }
         }
