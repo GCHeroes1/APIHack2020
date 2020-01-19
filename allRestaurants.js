30de501f76f1dd84a928cabcf1f88cd5dbfff90b
Alex <nathaliealexandra@gmail.com>
2020-01-19 12:28:53 +0000

    minor changes to location, and added distance calculating function

--- allRestaurants.js
+++ allRestaurants.js
@@ -93,22 +93,25 @@ function searchAll(){
 }
 
 var cord = document.getElementById("Location");
+var UserLat = 0;
+var UserLong = 0;
 function getLocation() {
     if (navigator.geolocation) {
-        console.log("Your browser supports geolocation");
+        //console.log("Your browser supports geolocation");
         navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
-        console.log("Geolocation is not supported by this browser.");
+        //console.log("Geolocation is not supported by this browser.");
         cord.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
 function showPosition(position) {
-    console.log("I get here");
-    console.log("Latitude: " + position.coords.latitude +
-        "\nLongitude: " + position.coords.longitude);
-    cord.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
+    UserLat = position.coords.latitude;
+    UserLong = position.coords.longitude;
+    /console.log("Latitude: " + UserLat +
+     //   "\nLongitude: " + UserLong);
+    cord.innerHTML = "Latitude: " + UserLat +
+        "<br>Longitude: " + UserLong;
 
 }
 
@@ -128,3 +131,21 @@ function showError(error) {
             break;
     }
 }
+/*
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {//this gets the distance in crow flies
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1);
+    var a =
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
+}*/
