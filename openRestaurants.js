19ca5f3748b7ec73a467fdb87b2a0d0e11b159a2
Rajesh Goyal <zcabrgo@ucl.ac.uk>
2020-01-19 10:56:50 +0000

    Searching open restaurants works

--- openRestaurants.js
+++ openRestaurants.js
@@ -48,3 +48,47 @@ function populateOpen() {
     });
 }
 
+function searchOpen(){
+    // Declare variables
+    let input;
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        let branches = response;
+        let template, tbody, clone, tgt, branchLocation;
+        tgt = document.querySelector("#contains-restaurants");
+        tgt.innerHTML = "";
+        for (let index = 0; index < branches.length; index++) {
+            let list_entry = branches[index];
+            // console.log(list_entry);
+            // for (let branch in list_entry){
+            restaurant = list_entry["restaurant_org"];
+            for (let branch in list_entry["restaurant_branches"]) {
+                input = document.getElementById('openInput').value;
+                //works for each branch name
+                branchLocation = list_entry["restaurant_branches"][branch]['branch_name'];
+                // openStatus = list_entry["restaurant_branches"][branch]["status"];
+
+                // Instantiate the table with the existing HTML tbody
+                // and the row with the template
+                template = document.querySelector('#restaurants');
+
+                // Clone the new row and insert it into the table
+                // tbody = document.querySelector("tbody");
+                console.log(input);
+                console.log(restaurant);
+                // console.log(currentBranch);
+                if (restaurant.includes(input)) {
+                    console.log(restaurant);
+                    clone = template.content.cloneNode(true);
+                    tbody = clone.querySelector("div.text-here");
+                    tbody.textContent = restaurant + " - " + branchLocation;
+                    // console.log(clone);
+
+                    // tgt = document.querySelector("#contains-restaurants");
+                    // tgt.innerHTML = "";
+                    tgt.appendChild(clone);
+                }
+            }
+        }
+    });
+}
